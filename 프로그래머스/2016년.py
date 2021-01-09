dfbd492a9f49656a7ef1255ab016828f6c81aee1
Jude Jeong <legagain@gmail.com>
2021-01-09 23:42:45 +0900

    2016년

---
+++ 프로그래머스/2016년.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    whatday = ["FRI","SAT","SUN","MON","TUE","WED","THU"]
+    month = [31,29,31,30,31,30,31,31,30,31,30]
+    if a==1:
+        answer = whatday[b%7-1]
+    else:
+        answer = whatday[(sum(month[:a-1])+b)%7-1]
+    return answer
