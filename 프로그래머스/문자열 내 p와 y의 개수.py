7bdeea69648e3b428e2abd3f48c794bff503f65c
jude0124 <legagain@gmail.com>
2021-01-13 15:13:13 +0900

    문자열 다루기 기본

--- 프로그래머스/문자열 내 p와 y의 개수.py
+++ 프로그래머스/문자열 내 p와 y의 개수.py
@@ -1,19 +1,21 @@
-def solution(s):
-    s2 = s.lower()
-    answer = True
-    cnt = 0
-    cnt2 = 0
-    for i in s2:
-        if i == "p":
-            cnt += 1
-        elif i == "y":
-            cnt2 += 1
-    if cnt != cnt2:
-        answer = False
-    return answer
+class countpandy:
+    def solution(self,s):
+        s2 = s.lower()
+        answer = True
+        cnt = 0
+        cnt2 = 0
+        for i in s2:
+            if i == "p":
+                cnt += 1
+            elif i == "y":
+                cnt2 += 1
+        if cnt != cnt2:
+            answer = False
+        return answer
 
 
-# 다른 사람의 풀이
-# count 함수 하나면 for문 생략 가능
-def solution(s):
-    return s.lower().count("p") == s.lower().count("y")
+    #  다른 사람의 풀이
+    #  count 함수 하나면 for문 생략 가능
+
+    def solution2(self,s):
+        return s.lower().count("p") == s.lower().count("y")
