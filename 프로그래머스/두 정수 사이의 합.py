0ec46c6bcc2c9f9f40c8cffd8535dd5c752ca082
jude0124 <legagain@gmail.com>
2021-01-13 13:53:15 +0900

    두 정수 사이의 합

---
+++ 프로그래머스/두 정수 사이의 합.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    answer = 0
+    if a == b :
+        answer = a
+    elif a > b :
+        answer = sum(x for x in range(b,a+1))
+    else:
+        answer = sum(x for x in range(a,b+1))
+    return answers
