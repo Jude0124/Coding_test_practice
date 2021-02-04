1b3feda00ad539a3a0beb68641e727914190ff3d
Jude Jeong <legagain@gmail.com>
2021-02-04 22:16:03 +0900

    불량 사용자

--- 프로그래머스/lv3/불량 사용자.py
+++ 프로그래머스/lv3/불량 사용자.py
@@ -1,18 +1,17 @@
 import re
 from itertools import product
+
 def solution(u, b):
-    tmp =[]
+    tmp,t =[],[]
     c =0
     for i in range(len(b)):
         b[i] = re.sub(r"\*","[a-z0-9]",b[i])+"$"
         tmp2 =[]
         for j in u:
             if re.match(b[i],j) != None:
-                tmp2.append(re.match(b[i],j).group())
-        
+                tmp2.append(re.match(b[i],j).group())  
         tmp.append(tmp2)
-        
-    t = []
+
     for i in list(product(*tmp)):
         if len(i) != len(set(i)):
             continue
