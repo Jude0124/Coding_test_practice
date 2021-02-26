29e35dd36d755fee5632b874e6144feb80a2975d
jude0124 <legagain@gmail.com>
2021-02-26 16:39:17 +0900

    기지국 설치

--- 프로그래머스/lv3/기지국 설치.py
+++ 프로그래머스/lv3/기지국 설치.py
@@ -4,7 +4,10 @@ def solution(n,s,w):
         now = s.pop(0)
         if base < now-w:
             cnt += (now - base + w)//(2*w+1)
-        base = now+w+1
-        if (s == []) and (n > now+w):
-            cnt += (n - now + w)//(2*w+1)
+        base = now+w+1    
+    if n > now+w:
+        cnt += (n - now + w)//(2*w+1)
     return cnt
+    
+# 최소 하나의 기지국은 설치되어 있기때문에 기지국들 사이에 빈공간에 필요한 최소한의 기지국 갯수를 모두 더해주어 출력
+# 수정사항 마지막 if문 while문 밖에서 처리해주어 효율성 up
