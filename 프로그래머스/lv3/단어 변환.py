7a9cca2b62c492ae1018d30cf0ec5fd97678a370
Jude Jeong <legagain@gmail.com>
2021-02-28 23:29:31 +0900

    단어 변환

---
+++ 프로그래머스/lv3/단어 변환.py
@@ -0,0 +1,17 @@
+def solution(b, t, w):
+    k,b = len(b),[[b]]
+    while b:
+        tmp = []
+        for i in w:
+            for m in b:
+                if i not in m:
+                    cnt = 0
+                    for j in range(k):
+                        if m[-1][j] != i[j]:
+                            cnt += 1
+                    if cnt == 1:
+                        tmp.append(m+[i])
+                        if i == t:
+                            return len(m)
+        b = tmp[:]
+    return 0
