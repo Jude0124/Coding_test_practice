7b2748d3e7b447aa37782113358d4eb38e5d1125
Jude Jeong <legagain@gmail.com>
2022-01-13 18:58:43 +0900

    counting star_lv1

---
+++ 프로그래머스/lv1/CountingBoxStar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class CountingBoxStar {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();   
+        String star = "";
+        
+        for(int i = 0 ; i < a ; i++){
+            star += "*";    
+        }
+        for(int j = 0 ; j < b ; j++){
+            System.out.println(star);
+        }
+    }
+}
