cee289513fe5d5068d95c5a7d4abf7e973e7d4b1
Jude <legagain@gmail.com>
2022-01-31 02:00:14 +0900

    AddAnotation

--- 프로그래머스/lv1/ReportResult.java
+++ 프로그래머스/lv1/ReportResult.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 public class ReportResult {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, myObject> reportLog = new HashMap<>();
+
+        Map<String, myObject> reportLog = new HashMap<>(); // key = 사용자 아이디, value = [신고받은횟수,사용자를 신고한 사람의 명단]
+
         ArrayList<Integer> preAns = new ArrayList<>();
+
         HashSet<String> report2 = new HashSet<>(Arrays.asList(report));
-        String[] repoLast = report2.toArray(new String[0]);
+
+        String[] repoLast = report2.toArray(new String[0]); // 빈 딕셔너리 생성
         for (String key : id_list){
             reportLog.put(key, new myObject());
         }
 
-        for (String repo : repoLast){
+        for (String repo : repoLast){                       //신고한 사람목록과 횟수 기입
             String[] temp = repo.split(" ");
             reportLog.get(temp[0]).repoTimes++;
             reportLog.get(temp[0]).reporter.add(temp[1]);
 
         }
 
-        for(String key : id_list){
+        for(String key : id_list){                          // 조회하면서 해당 사용자가 받아야할 메일 수를 cnt변수로 산출
             int cnt = 0;
             for(String value2: reportLog.get(key).reporter){
                 if (reportLog.get(value2).repoTimes>=k){
@@ -33,7 +37,7 @@ public class ReportResult {
         }
         return answer;
     }
-    static class myObject{
+    static class myObject{              // value값에 해당하는 객체 생성
         int repoTimes = 0;
         ArrayList<String> reporter = new ArrayList<>();
     }
