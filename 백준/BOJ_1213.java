5858ef96984d759e336384e5916359cddbebd241
Jude0124 <legagain@gmail.com>
2022-04-16 23:56:29 +0900

    create 팰린드롬 만들기

---
+++ 백준/BOJ_1213.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class BOJ_1213{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pelindrom = sc.nextLine();
+        int len = pelindrom.length();
+
+        for (int i = 0 ; i < len ; i ++ ){    
+            pelindrom.charAt(i);
+            char arr [] = pelindrom.toCharArray();
+
+        }
+
+        
+        System.out.println(pelindrom);
+        sc.close();
+    }
+
+
+}
