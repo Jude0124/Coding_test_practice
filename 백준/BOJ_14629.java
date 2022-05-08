f1f05e247bf864de20fbf332ab7c6cb74b4677ba
Jude0124 <legagain@gmail.com>
2022-05-08 23:59:11 +0900

    create 14629

---
+++ 백준/BOJ_14629.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ_14629 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long number = sc.nextLong();
+        
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        if (number >= 9876543210L){
+            System.out.println("9876543210");
+        } else{
+            
+        }
+    }   
+    
+}
